e72294d3c3d08db089c6b0794031785afd005b96
Brandon Hurler <brandonhurler@outlook.com>
2022-09-19 15:54:06 -0400

    Adding clean-deploy file standalone for use in branch-switching contexts.

---
+++ docker/sitecore/10.2/clean-deploy.ps1
@@ -0,0 +1,6 @@
+# Clean deploy folders
+Get-ChildItem -Path (Join-Path $PSScriptRoot "\deploy") -Directory | ForEach-Object {
+    $deployPath = $_.FullName
+
+    Get-ChildItem -Path $deployPath -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
+}
